bdd829c8c5d561fe137c5d3c7b407aa63463a813
T-PageCode Developer <cdt20260@outlook.com>
2026-08-29 12:52:54 +0800

    主页大换新，如果需要旧版本的主页访问index-old.html即可

---
+++ Script/index-old.js
@@ -0,0 +1,6 @@
+document.addEventListener("contextmenu",function(No_BrYMenu) {
+    No_BrYMenu.preventDefault()
+})
+document.addEventListener("selectstart",function(No_Selectstart) {
+    No_Selectstart.preventDefault()
+})
--- Script/index.js
+++ Script/index.js
@@ -1,6 +1,3 @@
-document.addEventListener("contextmenu",function(No_BrYMenu) {
-    No_BrYMenu.preventDefault()
-})
-document.addEventListener("selectstart",function(No_Selectstart) {
-    No_Selectstart.preventDefault()
+document.addEventListener("contextmenu",(e) => {
+    e.preventDefault();
 })
