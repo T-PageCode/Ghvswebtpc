66a038017eea3f24435b9636853128d5493511cd
T-PageCode Developer <cdt20260@outlook.com>
2026-09-21 18:07:42 +0800

    T-CodeSystem提示支持了关闭并且关闭效果是3D反转

--- Script/index.js
+++ Script/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("contextmenu",(e) => {
     e.preventDefault();
 })
+const closeButton = document.getElementById("close");
+const tCodeSystem = document.getElementById("t-codesystem")
+closeButton.onclick = () => {
+    tCodeSystem.style.transform = "perspective(1000px) rotateY(180deg) rotateX(180deg)";
+    setTimeout(() => {
+        tCodeSystem.style.transform = "translateY(130px) perspective(1000px) rotateY(180deg) rotateX(180deg)";
+    },500)
+}
